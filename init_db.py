9421b1b016af030365324ddf159f7780c5ca70a4
Le Anh Vu <vula@Macbook-Air.local>
2016-03-14 18:41:09 +0700

    Auto update

--- init_db.py
+++ init_db.py
@@ -1,13 +1,13 @@
-# import MySQLdb
-import mysql.connector as mariadb
+import MySQLdb
+# import mysql.connector as mariadb
 
-# db = MySQLdb.connect("localhost", "root", "toor", "banknote")
-db = mariadb.connect(user = "vula", password = "mat.khau.cua.vula", database = "vudb")
+db = MySQLdb.connect("localhost", "root", "toor", "banknote")
+# db = mariadb.connect(user = "vula", password = "mat.khau.cua.vula", database = "vudb")
 cursor = db.cursor()
 
 data = open('./rate/USD.txt', 'r').read()
 data = data.split('\n')
-insertNewRate = "INSERT INTO rate_exchange (base_banknote_id, rate_banknote_id, rate) VALUES(%s, %s, %s);"
+insertNewRate = "INSERT INTO update_exchange_rate (base_banknote_code, rate_banknote_code, rate) VALUES(%s, %s, %s);"
 for i in range(0,len(data),3):
 	value = (data[i], data[i+1], data[i+2])
 	cursor.execute(insertNewRate, value)
--- testHTTPServer.py
+++ testHTTPServer.py
@@ -11,77 +11,40 @@ import mysql.connector as mariadb
 import json
 
 
-PORT_NUMBER = 12055
+PORT_NUMBER = 12057
 
 #This class will handles any incoming request from
 #the browser 
 class myHandler(BaseHTTPRequestHandler):
-	trademark_path = pjoin(curdir, str(datetime.now().microsecond) + ".jpg")
 
 	#Handler for the GET requests
 	def do_GET(self):
-		# self.send_response(200)
-		# self.send_header('Content-type','text/html')
-		# self.end_headers()
-		# # Send the html message
-		# self.wfile.write("Hello World !")
-		if self.path == '/USDVND':
-			# db = MySQLdb.connect("localhost", "root", "toor", "banknote")
-			db = MySQLdb.connect(user="vula", password="mat.khau.cua.vu", database="vudb")
+		if self.path == '/update':
+			db = MySQLdb.connect(user="vula", password="mat.khau.cua.vula", database="vudb")
 			cursor = db.cursor()
-			query = "SELECT rate FROM rate_exchange WHERE base_banknote_id = 150 AND rate_banknote_id = 154;"
-			cursor.execute(query)
-			result = cursor.fetchone()
-			dic = {}
-			dic['USD - VND'] = float(result[0])
-			print dic
+			cursor.execute('SELECT * FROM update_exchange_rate')
+			rows = cursor.fetchall()
+			columns = [desc[0] for desc in cursor.description]
 			db.close()
+			result = []
+			if hasattr(rows, '__iter__'):
+				for row in rows:
+					row = dict(zip(columns, row))
+					result.append(row)
 			self.send_response(200)
 			self.send_header('Content-type','text/html')
 			self.end_headers()
-			self.wfile.write(json.dumps(dic))
-
-		elif self.path == '/USD':
-			# db = MySQLdb.connect("localhost", "root", "toor", "banknote")
-			db = MySQLdb.connect(user="vula", password="mat.khau.cua.vu", database="vudb")
-			cursor = db.cursor()
-			cursor.execute("SELECT rate_banknote_id, rate FROM rate_exchange WHERE base_banknote_id = 150;")
-			rate = cursor.fetchall()
-			cursor.execute("SELECT banknote_code FROM banknote")
-			code = cursor.fetchall()
-
-			self.send_response(200)
-			self.send_header('Content-type','text/html')
-			self.end_headers()
-			dic = {}
-			for i in range(168):
-				dic['USD' + code[i][0]] = float(rate[i][1])
-			self.wfile.write(json.dumps(dic))
-		return
-
-	def do_POST(self):
-		if self.path == '/trademark':
-			form = cgi.FieldStorage(
-				fp=self.rfile,
-				headers=self.headers,
-				environ={'REQUEST_METHOD':'POST', 'CONTENT_TYPE':self.headers['Content-Type'],})
-			filename = form['file'].filename
-			data = form['file'].file.read()
-			with open(self.trademark_path, "wb") as fh:
-				fh.write(data)
-			self.send_response(200)
-
-		elif self.path == '/moneyReader/':
-			form = cgi.FieldStorage(
-				fp=self.rfile,
-				headers=self.headers,
-				environ={'REQUEST_METHOD':'POST', 'CONTENT_TYPE':self.headers['Content-Type'],})
-
-
+			self.wfile.write(json.dumps(result, default=datetime_serial))
 		return
 
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
 	"""Handle requests in a separate thread."""
+
+def datetime_serial(obj):
+	if isinstance(obj, datetime):
+		serial = obj.isoformat()
+		return serial
+	raise TypeError("Type not serializable")
 
 if __name__ == '__main__':
 	try:
